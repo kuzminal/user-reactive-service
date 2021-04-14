7fda77077cb9326ff512ab7261676a13c40baf28
kuzminal <kuzmin.al35@gmail.com>
2021-04-14 18:32:43 +0300

    Add JWT authentication

--- src/main/java/com/kuzmin/userservice/config/SecurityConfig.java
+++ src/main/java/com/kuzmin/userservice/config/SecurityConfig.java
@@ -1,14 +1,39 @@
 package com.kuzmin.userservice.config;
 
+import com.kuzmin.userservice.jwt.JwtTokenAuthenticationFilter;
+import com.kuzmin.userservice.jwt.JwtTokenProvider;
+import com.kuzmin.userservice.repository.UserRepository;
+import com.kuzmin.userservice.service.ReactiveUserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.ReactiveAuthenticationManagerAdapter;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+    @Autowired
+    private ReactiveUserDetailsServiceImpl userDetailsService;
+    @Bean
+    public ReactiveAuthenticationManager authenticationManager() {
+        UserDetailsRepositoryReactiveAuthenticationManager authenticationManager =
+                new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
+        authenticationManager.setPasswordEncoder(passwordEncoder());
+        return authenticationManager;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -16,9 +41,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(
-            ServerHttpSecurity http) {
+            ServerHttpSecurity http,
+            JwtTokenProvider tokenProvider) {
         http.csrf().disable();
-        return http.authorizeExchange()
+        return http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .addFilterAt(new JwtTokenAuthenticationFilter(tokenProvider), SecurityWebFiltersOrder.HTTP_BASIC)
+                .authenticationManager(authenticationManager())
+                .authorizeExchange()
                 .pathMatchers("/users")
                 .authenticated()
                 .anyExchange()
